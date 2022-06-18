3e6eaf4d6156c5db54076028b38fd25fd462eb11
SummerLionTester <added basic UI style with ant design>
2022-06-18 16:56:48 +0300

    added downbeatSample use cases

---
+++ src/application/downbeatSample.js
@@ -0,0 +1,27 @@
+import createSample from "../domain/derivedEntities/sample/createSample";
+
+const downbeatSample = (options) => {
+    const sample = createSample(options.initialValue, options.available);
+
+    const set = (value) => {
+        if(sample.checkValue(value)) {
+            sample.set(value);
+        }
+    };
+
+    const subscribe = (key, getUpdateCallback, isNeedValueForInit) => {
+        sample.subscribe(key, getUpdateCallback, isNeedValueForInit);
+    };
+
+    const unsubscribe = (key) => {
+        sample.unsubscribe(key);
+    };
+
+    return Object.freeze({
+        set,
+        subscribe,
+        unsubscribe
+    });
+};
+
+export default downbeatSample;
