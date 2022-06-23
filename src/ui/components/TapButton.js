20d7aef6a380016eb106b03f000759b585b697a1
CurruptedMonk <dmitriy.petrov.1993@yandex.ru>
2022-06-23 17:23:33 +0300

    added TapButton component

--- src/ui/components/TapButton.js
+++ src/ui/components/TapButton.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Button} from "antd";
 import onTap from "../handlers/onTap";
 
 const TapButton = ({controller}) => {
+    const currentButtonRef = useRef(null);
+    const keyboardHandler = (e) => {
+        if (e.key.toLowerCase() === "t") {
+            currentButtonRef.current.click();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", keyboardHandler);
+
+        return () => {
+            window.removeEventListener("keydown", keyboardHandler);
+        };
+    }, [controller]);
+
     return (
-        <Button type="primary"
-                size={"large"}
-                onClick={() => onTap(controller.bpm.set)}
+        <Button
+            ref={currentButtonRef}
+            type="primary"
+            size={"large"}
+            onClick={() => onTap(controller.bpm.set)}
         >
             Tap BPM
         </Button>
