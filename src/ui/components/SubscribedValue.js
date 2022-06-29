eb40606c875b093308a918dfc6839170e12689ca
CurruptedMonk <dmitriy.petrov.1993@yandex.ru>
2022-06-29 15:38:36 +0300

    added SubscribedValue component

---
+++ src/ui/components/SubscribedValue.js
@@ -0,0 +1,10 @@
+import React from "react"
+import useSubscribe from "../hooks/useSubscribe";
+
+const SubscribedValue = ({ controller, ...props }) => {
+    const [value] = useSubscribe(controller);
+
+    return <span {...props}>{value}</span>;
+};
+
+export default SubscribedValue;
