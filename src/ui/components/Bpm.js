d5c0832d5b4b170a5597245904dbb56579235ad8
CurruptedMonk <dmitriy.petrov.1993@yandex.ru>
2022-06-29 16:55:55 +0300

    refactoring use SubscribedSlider in others components

--- src/ui/components/Bpm.js
+++ src/ui/components/Bpm.js
@@ -1,44 +1,37 @@
 import React from "react";
-import {Slider} from "antd";
 import {PlusOutlined, MinusOutlined} from "@ant-design/icons";
-import useSubscribe from "../hooks/useSubscribe";
 import HeldButton from "./HeldButton";
+import SubscribedSlider from "./SubscribedSlider";
 
 const Bpm = ({controller, bpmOptions}) => {
     const STEP = 1;
     const HELD_DELAY = 35;
 
-    const [bpm] = useSubscribe(controller.bpm);
-
     return (
-        <div>
-            <div style={{display: "flex", justifyContent: "center", textAlign: "center"}}>
-                <HeldButton
-                    callback={controller.bpm.decreaseBy.bind(null, STEP)}
-                    heldDelay={HELD_DELAY}
-                    size={"large"}
-                    type="primary"
-                    shape="circle"
-                    icon={<MinusOutlined />}
-                />
-                <Slider
-                    min={bpmOptions.range.from}
-                    max={bpmOptions.range.to}
-                    step={STEP}
-                    value={bpm}
-                    tooltipVisible={false}
-                    onChange={(bpm) => controller.bpm.set(parseInt(bpm))}
-                    style={{ width: "100%" }}
-                />
-                <HeldButton
-                    callback={controller.bpm.increaseBy.bind(null, STEP)}
-                    heldDelay={HELD_DELAY}
-                    size={"large"}
-                    type="primary"
-                    shape="circle"
-                    icon={<PlusOutlined />}
-                />
-            </div>
+        <div style={{display: "flex", justifyContent: "center", textAlign: "center"}}>
+            <HeldButton
+                callback={controller.bpm.decreaseBy.bind(null, STEP)}
+                heldDelay={HELD_DELAY}
+                size={"large"}
+                type="primary"
+                shape="circle"
+                icon={<MinusOutlined />}
+            />
+            <SubscribedSlider
+                style={{ width: "100%" }}
+                controller={controller.bpm}
+                min={bpmOptions.range.from}
+                max={bpmOptions.range.to}
+                step={STEP}
+            />
+            <HeldButton
+                callback={controller.bpm.increaseBy.bind(null, STEP)}
+                heldDelay={HELD_DELAY}
+                size={"large"}
+                type="primary"
+                shape="circle"
+                icon={<PlusOutlined />}
+            />
         </div>
     );
 };
